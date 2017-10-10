02ba566763fab3d6eca9a3ae741588f77c3cbbc0
Norbert Wiek <norbert.wiek@googlemail.com>
2017-10-10 03:11:38 -0700

    Updated Modul 3 - Exercise 2

--- Modul 3 - Exercise 2/ContosoConf/scripts/pages/register.js
+++ Modul 3 - Exercise 2/ContosoConf/scripts/pages/register.js
@@ -5,17 +5,28 @@ var submitButton = form.querySelector("button");
 // TODO: Task 1 - Get the password <input> elements from the DOM by ID
 // var passwordInput = ...;
 // var confirmPasswordInput = ...;
+var passwordInput = document.getElementById("password")
+var confirmPasswordInput = document.getElementById("confirm-password");
 
 var checkPasswords = function () {
     // TODO: Task 2 - Compare passwordInput value to confirmPasswordInput value
+    var passwordsMatch = passwordInput.value === confirmPasswordInput.value;
 
     // TODO: Task 3 - If passwords don't match then display error message on confirmPasswordInput (using setCustomValidity)
     //                If passwords do match then clear the error message (setCustomValidity with empty string)
+    if (passwordsMatch) {
+        confirmPasswordInput.setCustomValidity("");
+    }
+    else {
+        confirmPasswordInput.setCustomValidity("Passwords don't match. Please re-enter");
+    }
 };
 
 var addPasswordInputEventListeners = function () {
     // TODO: Task 4 - Listen for the "input" event on passwordInput and confirmPasswordInput.
     //       Call the checkPasswords function
+    passwordInput.addEventListener("input", checkPasswords, false);
+    confirmPasswordInput.addEventListener("input", checkPasswords, false);
 };
 
 var formSubmissionAttempted = function() {
