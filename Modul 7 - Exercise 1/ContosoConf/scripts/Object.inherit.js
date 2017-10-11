6deac8a17adc2c0fef4708f904c01b031a8fc113
Norbert Wiek <norbert.wiek@googlemail.com>
2017-10-11 04:57:18 -0700

    Updated Modul 7 - Exercise1

--- Modul 7 - Exercise 1/ContosoConf/scripts/Object.inherit.js
+++ Modul 7 - Exercise 1/ContosoConf/scripts/Object.inherit.js
@@ -1,43 +1,45 @@
 ﻿// TODO: Wrap this file's code in an immediately invoked function expression
-
-// TODO: Strict mode
-
-var copyOwnProperties = function (from, to) {
-    for (var propertyName in from) {
-        if (from.hasOwnProperty(propertyName)) {
-            to[propertyName] = from[propertyName];
+(function () {
+    // TODO: Strict mode
+    "use strict";
+    var copyOwnProperties = function (from, to) {
+        for (var propertyName in from) {
+            if (from.hasOwnProperty(propertyName)) {
+                to[propertyName] = from[propertyName];
+            }
         }
-    }
-};
+    };
 
-var inherit = function (additionalProperties) {
-    // `inherit` creates an factory object that is similar to a Class in C# and Java.
-    // The created factory object has a `create` method that creates instances.
-    // The factory object also serves as the prototype for created instances.
-    // So any properties added to the factory (via the additionalProperties parameter)
-    // will available to created instances.
-    // If the additionalProperties has a function named `initialize`, then this is
-    // called to initialize created instances.
-    
-    // TODO: Create a variable named `factory`, assign it a new object who's prototype is `this`.
-    
-    // TODO: Add a method called `create` to `factory`, that does the following
-    
-        // TODO: Define a variable named `instance` 
-        //       and assign it a new object that has `factory` as its prototype.
-    
-        // TODO: If `instance` has a function named "initialize",
-        //       then call `initialize`, passing any arguments passed to `create`.
-    
-        // TODO: return `instance`.
-    
+    var inherit = function (additionalProperties) {
+        // `inherit` creates an factory object that is similar to a Class in C# and Java.
+        // The created factory object has a `create` method that creates instances.
+        // The factory object also serves as the prototype for created instances.
+        // So any properties added to the factory (via the additionalProperties parameter)
+        // will available to created instances.
+        // If the additionalProperties has a function named `initialize`, then this is
+        // called to initialize created instances.
 
-    // TODO: Copy properties of `additionalProperties` onto `factory' (using copyOwnProperties).
-    
-    // TODO: Return the `factory` object.
-};
 
-// TODO: Add the inherit function to the built-in `Object` object.
+        var factory = new Object(this);
+
+        factoty.create = function () {
+            var instance = Object.create(factory);
+
+            if (typeof instance.initialize === "function") {
+                instance.initialize.apply(instance, arguments);
+
+                return instance;
+            }
+        };
+
+        copyOwnProperties(additionalProperties, factory);
+
+        return factory
+    };
+
+    Object.inherti = inherit;
+
+}());
 // SIG // Begin signature block
 // SIG // MIIaVgYJKoZIhvcNAQcCoIIaRzCCGkMCAQExCzAJBgUr
 // SIG // DgMCGgUAMGcGCisGAQQBgjcCAQSgWTBXMDIGCisGAQQB
